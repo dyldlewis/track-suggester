4007d8913543369482ab7f10bd68899c9a872f55
Dylan Lewis <dyldlewis@gmail.com>
2017-06-02 12:13:36 -0300

    add jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
     var business = $("input:radio[name=business]:checked").val();
   if (money === "yes") {
     alert("Programming is hard and requires passion, if you're doing it for money we reccomend you don't take any courses!")
-  } else if (interest === "software" && business === "company" ) {
+  } else if (interest === "software") {
     alert("Based on your answers, we reccomend you take the C#/.NET course")
   } else if (interest === "webDesign") {
     alert("Based on your answers, we reccomend you take the CSS/Design course")
+  } else if (interest === "webDev" && business === "myself") {
+    alert("Based on your answers, we reccomend you take the Ruby/Rails course")
   } else if (interest === "webDev") {
     alert("Based on your answers, we reccomend you take the PHP/React course")
   } else if (interest === "mobileDev") {
